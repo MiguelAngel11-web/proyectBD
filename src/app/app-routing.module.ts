6d9068d89a467f8c52cb37f364ee474bb9390b50
MiguelAngel11-web <miguel_asc11@hotmail.com>
2020-11-27 19:01:31 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { MatterComponent } from './matter/matter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+   {path:'login',component:LoginComponent},
+   {path:'teach',component:TeachersComponent},
+   {path:'matter',component:MatterComponent},
+  /*{path:'homeAdmin',component:HomeAdminComponent, */
+    /* canActivate:[RutasLimitadasGuard] }, */
+  /*Redireccionamientos al iniciar la app o un 404 */
+  {path: '',pathMatch:'full',redirectTo: 'home'},
+  {path: '**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatterComponent } from './matter/matter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    TeachersComponent,
+    FooterComponent,
+    MatterComponent
   ],
   imports: [
     BrowserModule,
